7818b2be70cb2ee48aab48238baa62ca3b764b9e
Visa Haarala <visa.haarala@gmail.com>
2024-03-24 13:43:56 +0200

    beats visual

--- src/pages/Chords.tsx
+++ src/pages/Chords.tsx
@@ -1,7 +1,7 @@
 import styles from './Chords.module.scss';
 import Chord from '../components/chords/Chord';
 import PlayControls from '../components/play-controls/PlayControls';
-// import Beats from '../components/beats/Beats';
+import Beats from '../components/beats/Beats';
 
 const Chords = () => (
   <div className={styles.chordPage}>
@@ -9,10 +9,9 @@ const Chords = () => (
       <Chord size={2} />
       <Chord indexOffset={1} size={0.9} contrast={40} />
     </div>
-
-    {/* <div className={styles.beats}>
+    <div className={styles.beats}>
       <Beats />
-    </div> */}
+    </div>
     <div className={styles.controls}>
       <PlayControls />
     </div>
