4e50795e819c5da6a6093709aeb38b2ce1b77a4f
Visa Haarala <visa.haarala@gmail.com>
2024-03-24 12:32:42 +0200

    fix info

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -7,14 +7,14 @@ const Info = () => (
       <p>A simple tool for practicing random jazz chords.</p>
       <p>
         Designed for portrait mobile phone, but also works on desktop. On
-        desktop, use <code>(shift-)tab</code>, <code>space</code>,
+        desktop, use <code>(shift-)tab</code>, <code>space</code>,{' '}
         <code>esc</code> and <code>arrow</code> keys for navigation while
         playing an instrument.
       </p>
       <p>
-        Play button might not work on older iOS versions because of
-        problems with AudioContext. Using tab to navigate on desktop might not
-        work in Safari.
+        Play button might not work on older iOS versions because of problems
+        with AudioContext. Using tab to navigate on desktop might not work in
+        Safari.
       </p>
       <p>
         More info at{' '}
