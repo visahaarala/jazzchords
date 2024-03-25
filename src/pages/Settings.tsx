147ca94605d350437944fb8bbeefc0cb0a04cecb
Visa Haarala <visa.haarala@gmail.com>
2024-03-25 15:31:42 +0200

    UI copy url, no functionality yet

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -6,7 +6,7 @@ import { Context } from '../context/Context';
 import { basesOrganized, extensionsOrganized } from '../data/harmonies';
 import { AccidentalLevel, ExtensionLevel } from '../@types';
 import MetronomeSelect from '../components/settings/MetronomeSelect';
-// import CopyIcon from '../components/svgs/settings/CopyIcon';
+import CopyIcon from '../components/svgs/settings/CopyIcon';
 
 const Settings = () => {
   const ctx = useContext(Context);
@@ -14,10 +14,10 @@ const Settings = () => {
   return (
     <>
       <h2>settings</h2>
-      {/* <div className={styles.copy}>
+      <div className={styles.copy}>
         <span>Copy settings url to clipboard</span>
         <CopyIcon />
-      </div> */}
+      </div>
       <div className={styles.settings}>
         <h3>metronome</h3>
         <MetronomeSelect />
