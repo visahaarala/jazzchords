ef566df9139332e91c7f303f87bf5440c313d9b6
Visa Haarala <visa.haarala@gmail.com>
2024-03-29 19:14:13 +0200

    testing with headers..

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -4,18 +4,21 @@ import PlayControls from '../components/player/PlayControls';
 import Beats from '../components/beats/Beats';
 
 const Play = () => (
-  <div className={styles.playPage}>
-    <div className={styles.chords}>
-      <Chord size={1.6} />
-      <Chord indexOffset={1} size={0.95} contrast={30} />
+  <>
+    {/* <h2>player</h2> */}
+    <div className={styles.playPage}>
+      <div className={styles.chords}>
+        <Chord size={1.6} />
+        <Chord indexOffset={1} size={0.95} contrast={30} />
+      </div>
+      <div className={styles.beats}>
+        <Beats />
+      </div>
+      <div className={styles.controls}>
+        <PlayControls />
+      </div>
     </div>
-    <div className={styles.beats}>
-      <Beats />
-    </div>
-    <div className={styles.controls}>
-      <PlayControls />
-    </div>
-  </div>
+  </>
 );
 
 export default Play;
