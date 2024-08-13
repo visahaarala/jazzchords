4b15737ac19376f593b02a52c213d4466d7cfac0
Visa Haarala <visa.haarala@gmail.com>
2024-08-13 18:14:11 +0300

    clean vercel errors

--- src/components/chords/Chords.tsx
+++ src/components/chords/Chords.tsx
@@ -2,30 +2,30 @@ import styles from './Chords.module.scss';
 import { useContext } from 'react';
 import { ReducerContext } from '../../context/ReducerContext';
 import Chord from './Chord';
-import A from './symbols/keys/A';
-import B from './symbols/keys/B';
-import C from './symbols/keys/C';
-import D from './symbols/keys/D';
-import E from './symbols/keys/E';
-import F from './symbols/keys/F';
-import G from './symbols/keys/G';
-import Flat from './symbols/extensions/Flat';
-import Plus from './symbols/extensions/Plus';
-import Sharp from './symbols/extensions/Sharp';
-import Minor from './symbols/keys/Minor';
-import Seven from './symbols/extensions/Seven';
-import Six from './symbols/extensions/Six';
-import Nine from './symbols/extensions/Nine';
-import Five from './symbols/extensions/Five';
-import Eleven from './symbols/extensions/Eleven';
-import Thirteen from './symbols/extensions/Thirteen';
-import Dim from './symbols/extensions/Dim';
-import HalfDim from './symbols/extensions/HalfDim';
-import Four from './symbols/extensions/Four';
-import Maj from './symbols/extensions/Maj';
-import Add from './symbols/extensions/Add';
-import Sus from './symbols/extensions/Sus';
-import Bracket from './symbols/extensions/Bracket';
+// import A from './symbols/keys/A';
+// import B from './symbols/keys/B';
+// import C from './symbols/keys/C';
+// import D from './symbols/keys/D';
+// import E from './symbols/keys/E';
+// import F from './symbols/keys/F';
+// import G from './symbols/keys/G';
+// import Flat from './symbols/extensions/Flat';
+// import Plus from './symbols/extensions/Plus';
+// import Sharp from './symbols/extensions/Sharp';
+// import Minor from './symbols/keys/Minor';
+// import Seven from './symbols/extensions/Seven';
+// import Six from './symbols/extensions/Six';
+// import Nine from './symbols/extensions/Nine';
+// import Five from './symbols/extensions/Five';
+// import Eleven from './symbols/extensions/Eleven';
+// import Thirteen from './symbols/extensions/Thirteen';
+// import Dim from './symbols/extensions/Dim';
+// import HalfDim from './symbols/extensions/HalfDim';
+// import Four from './symbols/extensions/Four';
+// import Maj from './symbols/extensions/Maj';
+// import Add from './symbols/extensions/Add';
+// import Sus from './symbols/extensions/Sus';
+// import Bracket from './symbols/extensions/Bracket';
 
 const Chords = () => {
   const isMobile = matchMedia('(pointer:coarse)').matches;
@@ -59,7 +59,6 @@ const Chords = () => {
         {/* <Six height={3} /> */}
         {/* <Five height={3} /> */}
         {/* <Eleven height={3} /> */}
-        {/* <Maj height={3} /> */}
         {/* <Add height={3} /> */}
         {/* <Sus height={3} /> */}
         {/* <Four height={3} /> */}
@@ -71,7 +70,9 @@ const Chords = () => {
         <Bracket height={10} flip />
         <Bracket height={10} double />
         <Bracket height={10} double flip /> */}
+        {/* <Maj height={10} /> */}
       </div>
+      {/* <Chord size={5} /> */}
       <Chord size={3.7} />
       <Chord indexOffset={1} size={2} contrast={50} />
     </div>
