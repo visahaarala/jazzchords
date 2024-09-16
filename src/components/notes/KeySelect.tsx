ec666a8b8698fb26c061112ecb6f1ec85a8702eb
Visa Haarala <visa.haarala@gmail.com>
2024-09-16 08:54:47 +0300

    fix vercel errors

--- src/components/notes/KeySelect.tsx
+++ src/components/notes/KeySelect.tsx
@@ -1,12 +1,8 @@
-import { useContext } from 'react';
 import { Accidental, Alphabet, Key } from '../../@types';
 import { keyToString, stringToKey } from '../../functions/chordFunctions';
 import Select from '../misc/Select';
-import { ChordsContext } from '../../context/ChordsContext';
 
 const KeySelect = () => {
-  const clef = useContext(ChordsContext).state.notesClef;
-
   const baseOptions: Alphabet[] = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 
   const accidentalOptions: Accidental[] = ['b', undefined, '#'];
@@ -28,23 +24,10 @@ const KeySelect = () => {
     keyOptions.map((ko) => ko.base + (ko.accidental ? ko.accidental : ''))
   );
 
-  // const optionsByClef = (
-  //   firstOption: string
-  // ): { base: Alphabet; accidental: Accidental }[] => {
-  //   const startIndex = keyOptions
-  //     .map((opt) => opt.base + (opt.accidental ? opt.accidental : ''))
-  //     .indexOf(firstOption);
-  //   return [
-  //     ...keyOptions.slice(startIndex),
-  //     ...keyOptions.slice(0, startIndex),
-  //   ];
-  // };
-
   return (
     <Select
       dispatchActionType='SET_NOTATION_KEY'
       payloadKey='notationKey'
-      // options={optionsByClef(clef === 'treble' ? 'F#' : 'E')}
       options={keyOptions}
       optionToString={keyToString}
       stringToOption={stringToKey}
